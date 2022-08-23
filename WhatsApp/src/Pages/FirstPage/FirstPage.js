c52466cfc3b6c982dc87bef8b16cf38b0197f486
Atakanz <atakanzer@gmail.com>
2022-08-23 20:13:23 +0300

    First Commit: Pages created.

---
+++ WhatsApp/src/Pages/FirstPage/FirstPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import styles from './FirstPage.style';
+
+
+
+const Main = ({navigation}) => {
+   
+
+    return (
+            <View>
+                <Text>Hello First!</Text>
+            </View>
+    )
+}
+
+export default Main;
---
+++ WhatsApp/src/Pages/MessagePage/MessagePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {SafeAreaView, View, Text} from "react-native";
+import styles from './MessagePage.style';
+
+const MessagePage = () => {
+    return (
+        <View>
+        <Text>Hello Messages!</Text>
+     </View>
+    )
+}
+
+export default MessagePage;
